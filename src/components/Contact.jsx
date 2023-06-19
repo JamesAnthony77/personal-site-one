2fe0c51054070f3e137998a19de934b70debe80d
James <jcotter1991@gmail.com>
2023-06-19 14:19:59 -0500

    Contact fixup

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,10 +11,10 @@ const Contact = () => {
       name="contact"
       className="w-full text-white md:h-screen bg-myCustomBackgroundColor flex items-center justify-center"
     >
-      <div>
+      <div className="mt-20">
         <Fade direction="up" duration={1300} triggerOnce>
           <button
-            className="text-white px-16 py-8 text-4xl my-2 flex items-center rounded-md bg-gradient-to-b from-purple-900 to-indigo-400 cursor-pointer shadow-md hover:shadow-lg duration-200 hover:scale-105"
+            className="text-white px-8 py-4 text-2xl md:px-16 md:py-8 md:text-4xl my-2 flex items-center rounded-md bg-gradient-to-b from-purple-900 to-indigo-400 cursor-pointer shadow-md hover:shadow-lg duration-200 hover:scale-105"
             onClick={handleEmailClick}
           >
             <span className="mr-2 font-aboutbutton text-shadow-sm">
